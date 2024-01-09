eb5d57e977b523c221ed6f927062d5dbd47be74c
Andrew R <andrew.r@exodus.io>
2024-01-09 11:46:24 -0500

    fix: formatDate build error

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date) {
-  const options = { month: '2-digit', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true };
+  const options: Intl.DateTimeFormatOptions = { month: '2-digit', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true };
   const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
   return formattedDate.replace(',', '');
 }
