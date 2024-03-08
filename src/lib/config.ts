eb2441694bfafa2f7853c4f6030d87e396d5fca9
Andrew R <83613934+andrewrexo@users.noreply.github.com>
2024-03-07 22:32:44 -0500

    feat: use production exchange server

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,9 +4,12 @@ const config = {
   appVersion: "1.0.0",
 };
 
-export const exodusV3Url = "https://exchange-s.exodus.io/v3";
+export const exodusV3Url = "https://exchange.exodus.io/v3";
 export const exodusPricingServerUrl = "https://pricing.a.exodus.io";
 
-export const baseUrl = process.env.NODE_ENV === "development" ? "http://127.0.0.1:3000/api" : "https://swap-iota-nine.vercel.app/api"
+export const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:3000/api"
+    : "https://swap-iota-nine.vercel.app/api";
 
 export default config;
